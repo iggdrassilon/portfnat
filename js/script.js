69732dc99d6938684cb2e6188980ea04b750d1fb
iggdrassilon <mr.undersson@gmail.com>
2022-12-01 15:54:25 +0300

    add project

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElement = document.querySelector('.menu__close'),
+    closeElementThree = document.querySelector('.menu__SwitchPage'),
+    closeElementTwo = document.querySelector('.menu__overlay');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElement.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+closeElementTwo.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+closeElementThree.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const percents = document.querySelectorAll('.persents__title-persent'),
+    scale = document.querySelectorAll('.persents__scale span');
+
+percents.forEach( (item, i) => {
+    scale[i].style.width = item.innerHTML;
+});
